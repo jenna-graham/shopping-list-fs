2dbd130c068e5594b764c82b713b64930f691e8f
jenna-graham <jclayto2@gmail.com>
2022-05-19 13:00:42 -0700

    added createListItem function in utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://icyrbwltlybmigeljxmh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljeXJid2x0bHlibWlnZWxqeG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzMDMxMzMsImV4cCI6MTk2Nzg3OTEzM30.kUzjWO4wi0A9AVcWdFt_BG9uq-HBZoAR2aZ4IRN55yw';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -35,6 +35,14 @@ export async function logout() {
     await client.auth.signOut();
 
     return (window.location.href = '../');
+}
+export async function createListItem(name, qty) {
+    const response = await client.from('shopping-items').insert({ name, qty });
+    if (response.error) {
+        console.error(response.error.message);
+    } else {
+        return response.data;
+    }
 }
 
 // function checkError({ data, error }) {
