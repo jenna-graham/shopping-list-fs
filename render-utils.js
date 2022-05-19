b10275dc9f311ac05555d59f7bf10e21e6310cc3
jenna-graham <jclayto2@gmail.com>
2022-05-19 15:04:57 -0700

    created renderList function

---
+++ render-utils.js
@@ -0,0 +1,10 @@
+export function renderList(item) {
+
+    const div = document.createElement('div'); 
+    div.textContent = `${item.qty} ${item.name}`;
+
+    if (item.purchased) {
+        div.classList.add('complete');
+    } 
+    return div;
+}
